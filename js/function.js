fec0dc6ebea40fd41feb3b3cf3cf52a329534980
marcosbk6 <marcos06roberto06@gmail.com>
2025-07-24 14:03:32 -0300

    agora aparece um personagem em cada cor, o fundo tb muda de acordo com a cor, e mostra o que foi dito pelo usuario

--- js/function.js
+++ js/function.js
@@ -10,16 +10,28 @@ var engine = {
         'grey': '#ebebeb',
         'black': '#141414',
     },
+    
+    imagens: {
+        '#02ef00': '../img/luigi.png',
+        '#790093': '../img/waluigi.png',
+        '#f02a7e': '../img/princesa-peach.png',
+        '#e90808': '../img/mario.png',
+        '#e7d703': '../img/wario.png',
+        '#f16529': '../img/bowser.png',
+        '#ebebeb': '../img/chain-chomp.png',
+        '#141414': '../img/banzai-bil.png'
+    },
 
     "moedas":0
 }
 
 const audioMoeda = new Audio('audio/audio_moeda.mp3');
-const audioErrou = new Audio('audio/audio_errou.mp3')
+const audioErrou = new Audio('audio/audio_errou.mp3');
+
 function sortearCor(){
     var indexCorSorteada = Math.floor(Math.random() * engine.cores.length);
-    var legendaCorDaCaixa = document.getElementById('cor-na-caixa');
     var nomeCorSorteada= engine.cores[indexCorSorteada];
+    var legendaCorDaCaixa = document.getElementById('cor-na-caixa');
 
     legendaCorDaCaixa.innerText = engine.cores[indexCorSorteada].toUpperCase();
 
@@ -29,9 +41,22 @@ function sortearCor(){
 
 function aplicarCorNaCaixa(nomeDaCor){
     var caixaDasCores = document.getElementById('cor-atual');
+    var fundoCor = document.getElementById('fundo');
+    var personagem = document.getElementById('personagem');
+
+    var corHex = engine.hexadecimais[nomeDaCor];
+
     caixaDasCores.style.backgroundColor = nomeDaCor;
     caixaDasCores.style.backgroundImage = "url('/img/caixa-fechada.png')";
     caixaDasCores.style.backgroundSize= "100%";
+
+    fundoCor.style.backgroundColor = nomeDaCor;
+
+    let imagemPersonagem = engine.imagens[nomeDaCor];
+    personagem.style.backgroundImage = `url('${imagemPersonagem}')`;
+    personagem.style.backgroundSize = "contain";
+    personagem.style.backgroundRepeat = "no-repeat";
+    personagem.style.backgroundPosition = "center";
 }
 
 function atualizaPontuacao(valor){
@@ -77,10 +102,13 @@ if(window.SpeechRecognition || window.webkitSpeechRecognition){
         btnGravador.style.color = "black";
     }
 
+var resposta = document.getElementById("pronuncia");
+
     gravador.onresult = function(event){
         trasncricaoAudio = event.results[0][0].transcript.toUpperCase();
         respostaCorreta = document.getElementById('cor-na-caixa').innerText.toUpperCase();
 
+        resposta.innerHTML = trasncricaoAudio;
 
         console.log(trasncricaoAudio);
         console.log(respostaCorreta);
